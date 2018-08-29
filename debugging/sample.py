672d06ed11717e1e78b1e9e64162e565dd15e954
pybokeh <pybokeh@gmail.com>
2018-08-29 18:59:06 -0400

    updated file

---
+++ debugging/sample.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def samplefunc():
+    var = 10
+    print("One. Line 1 ..!")
+    print("Two. Line 2 ..!")
+    out = add("summy", var)
+    print('Three. Line 3 .. Done!')
+    return out
+
+samplefunc()
