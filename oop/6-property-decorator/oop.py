78fa1e5c855bfbb5a8fdf4d1438d21cbee07b6a9
pybokeh <pybokeh@protonmail.com>
2020-08-25 22:01:01 -0400

    Added present and scikit-neuralnet to useful packages list and updated oop example

--- oop/6-property-decorator/oop.py
+++ oop/6-property-decorator/oop.py
@@ -1,4 +1,3 @@
-
 class Employee:
 
     def __init__(self, first, last):
@@ -12,10 +11,26 @@ class Employee:
     @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+        
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
 
 emp_1 = Employee('John', 'Smith')
 
+emp_1.fullname = 'Corey Schafer'
+
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
+
+del emp_1.fullname
